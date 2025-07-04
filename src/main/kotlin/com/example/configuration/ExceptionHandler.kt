839e3ce7b8d5712c8179a137a54300f7f9bd0432
ViniZaramello <vzaramello@uolinc.com>
2025-07-04 18:02:12 -0300

    refact: Remove linha tratamento desnecessario com exception

--- src/main/kotlin/com/example/configuration/ExceptionHandler.kt
+++ src/main/kotlin/com/example/configuration/ExceptionHandler.kt
@@ -3,7 +3,6 @@ package com.example.configuration
 import io.ktor.http.HttpStatusCode
 import io.ktor.server.application.Application
 import io.ktor.server.application.install
-import io.ktor.server.plugins.BadRequestException
 import io.ktor.server.plugins.statuspages.StatusPages
 import io.ktor.server.response.respond
 
@@ -11,9 +10,6 @@ fun Application.configureExceptionHandling() {
     install(StatusPages) {
         exception<IllegalArgumentException> { call, cause ->
             call.respond(HttpStatusCode.BadRequest, mapOf("message" to (cause.message ?: "Invalid request")))
-        }
-        exception<BadRequestException> { call, cause ->
-            call.respond(HttpStatusCode.BadRequest, mapOf("message" to cause.message))
         }
         exception<Throwable> { call, _ ->
             call.respond(HttpStatusCode.InternalServerError, mapOf("message" to "An unexpected error occurred"))
